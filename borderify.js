28539f60d4c8ddbe890bc1c14707c68e4485c50e
TrekTu <xephidriver@gmail.com>
2021-01-24 01:26:17 +0800

    Initial commit

---
+++ borderify.js
@@ -0,0 +1,11 @@
+document.body.style.border = "5px solid red";
+var button = document.createElement("button");
+button.innerHTML = "!!Go To Download!!";
+var body = document.getElementsByClassName("count")[0];
+body.appendChild(button);
+button.addEventListener ("click", function() {
+ let currentURL= window.location.href ;
+ let downloadURL="https://9xbud.com/"+currentURL;
+ window.open(downloadURL,'_blank');
+ //window.location.assign("https://9xbud.com/"+currentURL);
+});
